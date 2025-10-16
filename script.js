ccd695ac55ef104b454e6f4dd9a24f841a6ec3b6
vaishnavi-vsp-droid <vaishnavi9853@gmail.com>
2025-10-16 17:09:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('uploadForm').addEventListener('submit', function() {
+    document.getElementById('loading').style.display = 'block';
+    document.querySelector('.content').style.opacity = '0.5';
+});
